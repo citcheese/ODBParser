101be475ea1b09d75efbbf6992fa49cacf0263f3
Matteo Tomasini <dsfsl@yahoo.com>
2020-03-01 00:12:23 -0500

    Update ODBconfig.py
    changed some sample values

--- ODBconfig.py
+++ ODBconfig.py
@@ -10,17 +10,18 @@ SHODAN_API_KEY = ""
 basepath = r""
 
 #fields you want to make sure are in ES indices and MongoDb collections. Add your own! Or if you want to grab the DB no matter what fields are in there, leave this list blank. Will probably end up with lots of crap, but whatever
-typelist = ['email',"first_name","last_name","username","alias","firstname","lastname","social security number","surname","patient","password","emails","fullname","employee"]
+typelist = ['email',"username","alias","firstname","lastname","social security number","surname","patient","password","fullname","employee"]
 
 #choose number of fields from above that must be in index or collection. For example if you want to have index with field containing the word 'email' AND a field containing the word 'IP' you would pick 2.
 numfieldsrequired = 2
+
 
 #choose what Mongodb collection names you care about. Script will see if one of the terms below is in collection/index name,
 # e.g. if have term "client", script will pull index called "clients" or "client_data." Here some examples or leave blank if you don't care and want to evaluate all collections for fields you care about.
 collectionamesIwant = ["user","employee","patient","customer","client","payments","member","people"]
 
 #ignore indices that are prob just logs, feel free to add more items to this list. or delete some. whatever
-ESindicesdontwant = ["metricbeat-","filebeat-", "_sample_data", "apm-", "metrics__", "charts__",
+ESindicesdontwant = ["metricbeat-","filebeat-","packetbeat-","heartbeat-","_sample_data", "apm-", "metrics__", "charts__",
                    "access-log","logstash-"]
 
 #same as above but for ES, in case care about different things, I prefer to keep these blank but up to you. If you want same names as mongodb just set variable equal to collectionsIwant, e.g. ESindicesIwant = collectionamesIwant
