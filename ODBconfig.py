2b9dc1902d5169820649e52737b90d696de6f102
Matteo Tomasini <dsfsl@yahoo.com>
2020-03-01 19:48:55 -0500

    Update ODBconfig.py

--- ODBconfig.py
+++ ODBconfig.py
@@ -1,7 +1,7 @@
 """
-##################
-Do what it says.
-##################
+###################
+# Do what it says #
+###################
 """
 #put your Shodan key here
 SHODAN_API_KEY = ""
@@ -15,14 +15,13 @@ typelist = ['email',"username","alias","firstname","lastname","social security n
 #choose number of fields from above that must be in index or collection. For example if you want to have index with field containing the word 'email' AND a field containing the word 'IP' you would pick 2.
 numfieldsrequired = 2
 
-
 #choose what Mongodb collection names you care about. Script will see if one of the terms below is in collection/index name,
 # e.g. if have term "client", script will pull index called "clients" or "client_data." Here some examples or leave blank if you don't care and want to evaluate all collections for fields you care about.
 collectionamesIwant = ["user","employee","patient","customer","client","payments","member","people"]
 
 #ignore indices that are prob just logs, feel free to add more items to this list. or delete some. whatever
 ESindicesdontwant = ["metricbeat-","filebeat-","packetbeat-","heartbeat-","_sample_data", "apm-", "metrics__", "charts__",
-                   "access-log","logstash-"]
+                   "access-log","logstash-","-logs","-log-","monitoring","alerts","prod-java","prod-php","reporttrafficinfo"]  
 
 #same as above but for ES, in case care about different things, I prefer to keep these blank but up to you. If you want same names as mongodb just set variable equal to collectionsIwant, e.g. ESindicesIwant = collectionamesIwant
 ESindicesIwant=[]
